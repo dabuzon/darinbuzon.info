bd138ee7e79d8df5642819de96573468b981e620
Darin Buzon <dabuzon@s-169-232-86-177.resnet.ucla.edu>
2018-12-06 22:33:53 -0800

    Rebuilt and added spotify

---
+++ src/scripts/index.js
@@ -0,0 +1,13 @@
+/* --- LIBRARIES --- */
+import 'jquery';
+import 'dayjs';
+
+/* --- ASSETS --- */
+import '../styles/main.scss';
+import '../index.html';
+
+/* --- MODULES --- */
+import './clock';
+//import './slideshow';
+//import './next-page';
+import './pageLoad';
---
+++ src/scripts/pageLoad.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('body').removeClass("faded-out");
+    $('body').hide().fadeIn('750');
+})
+
+export {default} from './pageLoad';
+export * from './pageLoad';
