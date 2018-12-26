e7873e4002b7951bcd05f539d7c57072ab00fe50
Darin Buzon <dabuzon@Darins-iMac.local>
2018-12-25 16:55:21 -0800

    Fixed display issue(?)

--- src/scripts/switch.js
+++ src/scripts/switch.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     if ($('.carousel img').hasClass('first')) {
         $('.carousel img.first').addClass('selected');
-        $('.selected').show();
     }
     if ($('.description p').hasClass('first')) {
         $('.description p.first').addClass('selected');
@@ -12,8 +11,4 @@ $(document).ready(function () {
         $('.selected').removeClass('selected').addClass('hide');
         
     })
-
-    $('.carousel .hide').hide();
-    $('.carousel .first.hide').hide();
-    $('.description p').hide();
 })
