aaab17bc7682f06ccd0a9097c5f1fa0a6ecfd348
Darin Buzon <dabuzon@darins-mbp.wireless.llu.ad.lluahsc.org>
2019-06-16 19:33:57 -0700

    Need to fix unrendered HTML in react

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,43 @@ import { Container } from "../Styles";
 
 import Header from "../components/Header";
 import Entry from "../components/Entry";
+import Texts from "../components/Texts";
 
-import Application from "../assets/img/Guggenheim/Application.jpg"
-import Questions from "../assets/img/Posters/10Q.jpg"
+import Application from "../assets/img/Guggenheim/Application.jpg";
+import Questions from "../assets/img/Posters/10Q.jpg";
+import MachinePoetry from "../assets/img/Machine Poetry/MachinePoetry.png";
+import Books from "../assets/img/Books/Books.png";
 
 export class Home extends Component {
   render() {
     return (
-      <Container pt="55px" pb="55px">
+      <Container pt="55px">
         <Header />
-        <Entry sauce={Application} name="Guggenheim" info="Development and implementation of a comprehensive style guide to address the museum's need for a more accessible web interface for visitors." ppl="In collaboration with and special thanks to Ben Kasum as well as the rest of the Use All Five family."/>
-        <Entry name="Machine Poetry" info="An interdiscplinary artistic reaction in subjects of programming, graphic design, and poetry. Senior capstone project in fulfillment of the requirements for the degree of Bachelor of Arts in Design | Media Arts at the University of California, Los Angeles." ppl="In collaboration with and special thanks to Ben Lerchin, Hye Min Cho, Casey Reas, and the whole struggle that was four years of an undergraduate art education."/>
-        <Entry sauce={Questions} name="Posters" info="Commissions, assignments, and self-initiated undertakings within the medium of posters. Clients included UCLA Herb Alpert School of Music, Use All Five, and UCLA Design | Media Arts." ppl="In collaboration with and special thanks to Willem Henri Lucas, Troy Kreiner, and most importantly Israel Gutierrez."/>
-        <Entry name="Books" info="Responses to assignments, topics, and phenomena, packaged through typographic and editorial approaches." ppl="In collaboration with and special thanks to Willem Henri Lucas, Rebeca Méndez, and as always Israel Gutierrez."/>
+        <Entry
+          sauce={Application}
+          name="Guggenheim"
+          info={Texts.guggz[0]}
+          ppl={Texts.guggz[1]}
+        />
+        <Entry
+          sauce={MachinePoetry}
+          name="Machine Poetry"
+          info={Texts.mp[0]}
+          ppl={Texts.mp[1]}
+        />
+        <Entry
+          sauce={Questions}
+          name="Posters"
+          info={Texts.p[0]}
+          ppl={Texts.p[1]}
+        />
+        <Entry
+          sauce={Books}
+          name="Books"
+          info={Texts.b[0]}
+          ppl={Texts.b[1]}
+        />
+        <Container></Container>
       </Container>
     );
   }
