9e558656d2d531d09428c42c981b3a494926db75
Darin Buzon <hello@darinbuzon.info>
2020-04-26 18:58:40 -0700

    Need to go over this repo; fragments.js needs to be deleted, see contentful error

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,21 +1,22 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import Head from '../components/head';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <Head title="Home" />
+      <h1>Hello.</h1>
+      <h2>
+        I'm Andrew, a full-stack developer living in beautiful Philadelphia.
+      </h2>
+      <p>
+        Need a developer? <Link to="/contact">Contact me.</Link>
+      </p>
+    </Layout>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
