49aadbc9c83dbbf00c070728643e67b576157691
Darin Buzon <hello@darinbuzon.info>
2020-04-27 12:07:28 -0700

    Modified dependencies; still having trouble with Vercel reading env var

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+const config = require('./src/data/config');
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: 'Full-Stack Bootcamp',
@@ -5,14 +11,16 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-source-contentful',
       options: {
-        spaceId: 'yzwpt0ue6tq4',
-        accessToken: '8_0PtOR9AcWn6oS-8Jj66KsrMmYwlTtdQUtPPrV2H00',
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
-    'gatsby-plugin-sass',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -20,7 +28,6 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-transformer-remark',
       options: {
@@ -34,6 +41,16 @@ module.exports = {
             },
           },
         ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: config.defaultTitle,
+        short_name: config.defaultTitle,
+        start_url: '/',
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
       },
     },
   ],
---
+++ src/data/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  defaultTitle: 'Darin Buzon',
+  logo: '',
+  author: 'Darin Buzon',
+  url: 'https://darinbuzon.info',
+  legalName: 'Darin Buzon',
+  defaultDescription: '',
+  socialLinks: {
+    twitter: '',
+    github: '',
+    linkedin: '',
+    instagram: '',
+  },
+  themeColor: '#00c6ff',
+  backgroundColor: '#00c6ff',
+  contact: {
+    email: 'hello@darinbuzon.info',
+    phone: '+1-909-855-1197',
+  },
+};
