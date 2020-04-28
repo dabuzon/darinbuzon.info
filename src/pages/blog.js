d82a84e1fe14e6cdcbb1ec4a2f1094b4ca301850
Darin Buzon <hello@darinbuzon.info>
2020-04-27 21:59:24 -0700

    Mostly structured out; currently blocked on designs

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,6 +6,8 @@ import blogStyles from './blog.module.scss';
 import Head from '../components/head';
 
 const BlogPage = () => {
+  // `data` is what gets the data we need from GraphQL!
+  // This is our query that fetches the data we want from Contentful
   const data = useStaticQuery(graphql`
     query {
       allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
@@ -20,6 +22,8 @@ const BlogPage = () => {
     }
   `);
 
+  // Here is how we want our component rendered
+  // We could use styles to just have this just a few components listed
   return (
     <Layout>
       <Head title="Blog" />
