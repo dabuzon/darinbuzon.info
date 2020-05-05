cedad3dbba1e0781fd6334d6b4f4993b01c89b4f
Darin Buzon <hello@darinbuzon.info>
2020-05-04 23:29:43 -0700

    Got the rich text to render

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import { useStaticQuery, Link } from 'gatsby';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import styles from './hero.module.scss';
 
 const Hero = () => {
   const data = useStaticQuery(graphql`
     query {
-      contentfulHero {
-        primaryLabel
-        labels
-        impactText
-        supportingText {
-          supportingText
+      allContentfulHero {
+        nodes {
+          primaryLabel
+          labels
+          impactText
+          supportingText {
+            json
+          }
         }
       }
     }
   `);
 
-  return <div></div>;
+  return (
+    <div>
+      {data.allContentfulHero.nodes.map((edge) => {
+        return (
+          <ul>
+            <li>{edge.primaryLabel}</li>
+            <li>{edge.labels}</li>
+          </ul>
+        );
+      })}
+
+      {data.allContentfulHero.nodes.map((edge) => {
+        return <h1>{edge.impactText}</h1>;
+      })}
+
+      {data.allContentfulHero.nodes.map((edge) => {
+        return <p>{documentToReactComponents(edge.supportingText.json)}</p>;
+      })}
+    </div>
+  );
 };
 
 export default Hero;
