9fd76c79450985bd655a18a1b57d604b6f7993d8
Darin Buzon <hello@darinbuzon.info>
2020-05-08 21:41:28 -0700

    index.js now takes Hero.js as it functionally should but styling is still broken; see Hero.js for the commented errors

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,58 +1,30 @@
 import React from 'react';
-import { useStaticQuery, Link } from 'gatsby';
+import { Link } from 'gatsby';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-
 import cx from 'classnames';
 
 import styles from './hero.module.scss';
 
 const Hero = (props) => {
-  const data = useStaticQuery(graphql`
-    query {
-      allContentfulHero {
-        nodes {
-          location
-          primaryLabel
-          labels
-          impactText
-          supportingText {
-            json
-          }
-        }
-      }
-    }
-  `);
-
-  const fetcher = data.allContentfulHero.nodes;
-  const correctData = fetcher.filter((input) =>
-    input.location.includes(`${props.location}`)
-  );
-
   return (
-    <div>
-      {correctData.map((edge) => {
-        return (
-          <div
-            className={cx({
-              [styles.index]: (correctData.location = 'Index'),
-              [styles.hero]: correctData.location != 'Index',
-            })}
-          >
-            <ul>
-              <Link className={styles.pills}>{edge.primaryLabel}</Link>
-              {edge.labels.map((label) => {
-                return <Link className={styles.pills}>{label}</Link>;
-              })}
-            </ul>
-            <div className={styles.grid}>
-              <h1>{edge.impactText}</h1>
-            </div>
-            <div className={styles.grid}>
-              <p>{documentToReactComponents(edge.supportingText.json)}</p>
-            </div>
-          </div>
-        );
-      })}
+    <div
+    //   className={cx({
+    //     [styles.index]: (props.location = 'Index'),
+    //     [styles.hero]: props.location != 'Index',
+    //   })}
+    >
+      <ul>
+        <Link className={styles.pills}>{props.primaryLabel}</Link>
+        {props.labels.map((label) => {
+          return <Link className={styles.pills}>{label}</Link>;
+        })}
+      </ul>
+      <div className={styles.grid}>
+        <h1>{props.impactText}</h1>
+      </div>
+      {/* <p className={styles.grid}>
+        {documentToReactComponents(props.supportingText.supportingText)}
+      </p> */}
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import { useStaticQuery } from 'gatsby';
+import { graphql } from 'gatsby';
 
 import { Layout, SEO, Hero } from 'components';
 
-const IndexPage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allContentfulTemplateIndex {
-        nodes {
-          heroes {
-            location
-          }
-        }
-      }
-    }
-  `);
+const IndexPage = ({ data }) => {
+  /* 
+    Data is being fetched here
+  */
+  const dataFetch = data.contentfulTemplateIndex.heroes;
+  console.log(dataFetch);
+
+  /* 
+    With the data fetched, we then pass it into props.
+    This algorithm does not account for individual objects
+    our populated array acquires
+
+    However, this generalized props declaration allows the
+    type of data to be consistent when passed into Hero.js
+  */
+  // let props = {
+  //   impactText: dataFetch.impactText,
+  //   labels: dataFetch.labels,
+  //   primaryLabel: dataFetch.primaryLabel,
+  //   location: dataFetch.location,
+  //   // supportingText: dataFetch.supportingText.json
+  // };
 
   return (
     <Layout>
-      <Hero location="Index" />
+      {dataFetch.map((edge) => {
+        return (
+          <Hero
+            impactText={edge.impactText}
+            labels={edge.labels}
+            primaryLabel={edge.primaryLabel}
+            location={edge.location}
+            supportingText={edge.supportingText.supportingText}
+          />
+        );
+      })}
     </Layout>
   );
 };
+
+export const query = graphql`
+  query {
+    contentfulTemplateIndex {
+      heroes {
+        image {
+          fluid {
+            src
+          }
+        }
+        impactText
+        labels
+        location
+        primaryLabel
+        supportingText {
+          supportingText
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
