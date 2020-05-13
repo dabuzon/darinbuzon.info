fdfcfeb4ed593560c2195bcc20f0cf7e50134665
Darin Buzon <hello@darinbuzon.info>
2020-05-13 14:38:50 -0700

    Apparently Case Study can't be written as a class component

--- src/templates/CaseStudy.js
+++ src/templates/CaseStudy.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import { graphql } from 'gatsby';
+import { get as _get } from 'lodash';
 
 import { Layout, SEO, Hero } from 'components';
 
-export class CaseStudy extends Component {
-  render() {
-    return <Layout></Layout>;
+const CaseStudy = (props) => {
+  return <Layout>{props.data.entry.title}</Layout>;
+};
+
+export const query = graphql`
+  query($slug: String!) {
+    entry: contentfulPageEntry(slug: { eq: $slug }) {
+      id
+      title
+      contentBlocks {
+        impactText
+        info {
+          info
+        }
+        labels
+        location
+        primaryLabel
+        sys {
+          contentType {
+            sys {
+              contentful_id
+            }
+          }
+        }
+      }
+    }
   }
-}
-
-// export const query = graphql`
-
-// `
+`;
 
 export default CaseStudy;
