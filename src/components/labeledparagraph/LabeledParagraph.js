5de9cd603b4160c4c66791ab13ba553efeff0546
Darin Buzon <hello@darinbuzon.info>
2020-05-14 22:07:01 -0700

    Need to think about separate takeaway component and image conditional

--- src/components/labeledparagraph/LabeledParagraph.js
+++ src/components/labeledparagraph/LabeledParagraph.js
@@ -5,12 +5,22 @@ import styles from './labeledparagraph.module.scss';
 
 const LabeledParagraph = (props) => {
   return (
-    <React.Fragment>
-      <div className={styles.pills}>{props.labels}</div>
-      <div className={styles.lists}>
-        {documentToReactComponents(props.copy)}
+    <div className={styles.container}>
+      <div>
+        <div>
+          {props.labels.map((labels, index) => {
+            if (index > 0) {
+              return <div className={styles.auxPills}>{labels}</div>;
+            } else {
+              return <div className={styles.pills}>{labels}</div>;
+            }
+          })}
+        </div>
+        <div className={styles.lists}>
+          {documentToReactComponents(props.copy)}
+        </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
