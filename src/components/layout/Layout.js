2bace439462bc87cd9c2045cb5e379524b70a3be
Darin Buzon <hello@darinbuzon.info>
2020-05-25 10:42:06 -0700

    Switched to different branch to work on 404 error

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,87 +1,116 @@
-import React from 'react';
-import { Link, useStaticQuery } from 'gatsby';
+import React, { Component } from 'react';
+import { Link, graphql } from 'gatsby';
+import { get as _get } from 'lodash';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import cx from 'classnames';
 
 import GlobalStyle from '../GlobalStyle';
 import styles from './layout.module.scss';
 
-const Layout = (props) => {
-  const data = useStaticQuery(graphql`
-    query {
-      footer: allContentfulLabeledParagraph(
-        filter: { contentType: { eq: "Footer" } }
-        sort: { fields: title }
-      ) {
-        nodes {
-          title
-          copy {
-            json
-          }
-        }
-      }
-      caseStudy: allContentfulPageCaseStudy {
-        nodes {
-          slug
+export class Layout extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      foot: _get(props, ['data', 'footer', 'nodes']),
+      cases: _get(props, ['data', 'caseStudy', 'nodes']),
+    };
+  }
+
+  sluglink = () => {
+    this.state.foot
+      .filter(
+        (urlPath) => urlPath.slug !== this.props.location.pathname.substr(1)
+      )
+      .map((result) => result.slug);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <GlobalStyle />
+        <div className={styles.container}>
+          <header>
+            <p className={styles.footlink}>{this.props.title}</p>
+            {/* 
+                 WE WILL REACTIVATE THIS ONCE WE GET TIME TO CODE OUT THE WRITING INDEX
+                 FOR NOW IT IS ONLY A NICE-TO-HAVE
+              */
+            /* <Link className="footlink" to={`/${props.pathReturn.toLowerCase()}`}>
+              {props.pathReturn}
+            </Link> */}
+          </header>
+          {this.props.children}
+          <footer>
+            {this.props.isIndex ? (
+              <div className={styles.footTopContainer}>
+                {this.props.isIndex
+                  ? this.state.foot.map((edge) => {
+                      return (
+                        <div>
+                          <div className="pills">{edge.title}</div>
+                          <div className={styles.lists}>
+                            {documentToReactComponents(edge.copy.json)}
+                          </div>
+                        </div>
+                      );
+                    })
+                  : false}
+              </div>
+            ) : (
+              false
+            )}
+            {!this.props.isIndex ? (
+              <div className={styles.entry}>
+                <Link className="footlink" to="/">
+                  Return
+                </Link>
+                {!this.props.isFour ? (
+                  <Link
+                    className="footlink"
+                    to={
+                      this.props.caseStudy
+                        ? `/${
+                            this.sluglink()[
+                              Math.floor(Math.random() * this.sluglink().length)
+                            ]
+                          }`
+                        : false
+                    }
+                  >
+                    Next case study &#8594;
+                  </Link>
+                ) : (
+                  false
+                )}
+              </div>
+            ) : (
+              false
+            )}
+          </footer>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export const query = graphql`
+  query {
+    footer: allContentfulLabeledParagraph(
+      filter: { contentType: { eq: "Footer" } }
+      sort: { fields: title }
+    ) {
+      nodes {
+        title
+        copy {
+          json
         }
       }
     }
-  `);
-
-  let pathCheck = data.caseStudy.nodes.filter(
-    (urlPath) => urlPath.slug !== props.location.pathname.substr(1)
-  );
-
-  let footTop;
-  if (props.isIndex) {
-    footTop = data.footer.nodes.map((edge) => {
-      return (
-        <div>
-          <div className="pills">{edge.title}</div>
-          <div className={styles.lists}>
-            {documentToReactComponents(edge.copy.json)}
-          </div>
-        </div>
-      );
-    });
+    caseStudy: allContentfulPageCaseStudy {
+      nodes {
+        slug
+      }
+    }
   }
-
-  return (
-    <React.Fragment>
-      <GlobalStyle />
-      <div className={styles.container}>
-        <header>
-          <p className={styles.footlink}>{props.title}</p>
-          <Link className="footlink" to={`/${props.pathReturn.toLowerCase()}`}>
-            {props.pathReturn}
-          </Link>
-        </header>
-        {props.children}
-        <footer>
-          {props.isIndex ? (
-            <div className={styles.footTopContainer}>{footTop}</div>
-          ) : (
-            false
-          )}
-          {!props.isIndex ? (
-            <div className={styles.entry}>
-              <Link className="footlink" to="/">
-                Return
-              </Link>
-              <Link
-                className="footlink"
-                to={`/${pathCheck.map((result) => result.slug)}`}
-              >
-                Next case study &#8594;
-              </Link>
-            </div>
-          ) : (
-            false
-          )}
-        </footer>
-      </div>
-    </React.Fragment>
-  );
-};
+`;
 
 export default Layout;
