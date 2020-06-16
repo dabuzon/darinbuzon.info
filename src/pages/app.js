4bb0429a19ffdf2c3a104dfa7d5e11db6003c029
Darin Buzon <hello@darinbuzon.info>
2020-06-15 22:41:38 -0400

    App routing seems to be working fine

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Router } from '@reach/router';
+import { useStaticQuery, graphql } from 'gatsby';
+
+import PrivateRoute from '../components/privateroute';
+import Layout from '../components/layout';
 import Login from '../components/Login';
 
-const App = () => (
-  <Layout>
-    <Router>
-      <PrivateRoute path="/app/details" component={Details} />
-      <Login path="/app/login" />
-    </Router>
-  </Layout>
-);
+const App = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      cases: allContentfulPageCaseStudy {
+        nodes {
+          slug
+        }
+      }
+    }
+  `);
+
+  return (
+    <Layout>
+      <Router>
+        {data.cases.nodes.map((edge) => {
+          return <PrivateRoute path={`/${edge}`} />;
+        })}
+        <Login path="/app/login" />
+      </Router>
+    </Layout>
+  );
+};
 
 export default App;
